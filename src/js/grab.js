fbefa07d370abff59b3bb5f3c084a1a648aaaa3c
SimoBekacem <125382745+SimoBekacem@users.noreply.github.com>
2023-06-13 14:46:51 +0100

    make the grab and the parameters compenents

--- src/js/grab.js
+++ src/js/grab.js
@@ -2,7 +2,13 @@ import React from 'react';
 const Grab = () => {
     return (
         <div className='Grab'>
-            <h1>Grab</h1>
+            <div className='Grab__title'>
+                <h1>Unlimited download! Starting from just $15/m</h1>
+            </div>
+            <div className='Grab__btn'>
+                <button>Grab new!</button>
+                <i class="fa-solid fa-xmark"></i>
+            </div>
         </div>
     )
 }
--- src/js/parameters&links.js
+++ src/js/parameters&links.js
@@ -2,7 +2,34 @@ import React from 'react';
 const ParametersAndLinks = () => {
     return (
         <div className='ParametersAndLinks'>
-            <h1>ParametersAndLinks</h1>
+            <div className='ParametersAndLinks__links'>
+                <a href='#' target='_blank' rel="noopener noreferrer">About</a>
+                <a href='#' target='_blank' rel="noopener noreferrer">Forum</a>
+                <a href='#' target='_blank' rel="noopener noreferrer">Bay now!</a>
+                <a href='#' target='_blank' rel="noopener noreferrer">Login / Join</a>
+            </div>
+            <div className='ParametersAndLinks__parameters'>
+                <div className='ParametersAndLinks__parameters__item'>
+                    <h1>A-</h1>
+                    <h1>A</h1>
+                    <h1>A+</h1>
+                </div>
+                <div className="dropdown-center">
+                    <button className="btn btn-secondary" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i className="fa-solid fa-circle-half-stroke"></i>
+                    </button>
+                    <ul className="dropdown-menu">
+                        <li><i class="fa-regular fa-sun"></i><p>Light</p></li>
+                        <li><i class="fa-solid fa-moon"></i><p>Dark</p></li>
+                        <li><i className="fa-solid fa-circle-half-stroke"></i><p>Auto</p></li>
+                    </ul>
+                </div>
+                <a href="#"><i className="fa-brands fa-square-facebook"></i></a>
+                <a href=""><i className="fa-brands fa-square-instagram"></i></a>
+                <a href=""><i className="fa-brands fa-square-twitter"></i></a>
+                <a href=""><i className="fa-brands fa-youtube"></i></a>
+                <a href=""><i className="fa-brands fa-square-vimeo"></i></a>
+            </div>
         </div>
     )
 }
