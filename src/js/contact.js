03b6922221e5e1ba9aafb2b7fd23aea9d2c0e19b
SimoBekacem <125382745+SimoBekacem@users.noreply.github.com>
2023-06-15 09:57:56 +0100

    files added

--- src/js/contact.js
+++ src/js/contact.js
@@ -2,7 +2,19 @@ import React from 'react';
 const Contact = () => {
     return (
         <div className='Contact'>
-            <h1>Contact</h1>
+            <div className='Contact__logo'>
+                <img src={require('../images/logo.png')} width={"50px"}/>
+                <h1>ZINE</h1>
+            </div>
+            <p className='Contact__text'>The next-generation blog, news, and magazine theme for you to start sharing your stories today! This Bootstrap 5 based theme is ideal for all types of sites that deliver the news.</p>
+            <div className='Contact__form'>
+                <form>
+                    <input type='email' placeholder='Your Email' />
+                    <button>Subscribe</button>
+                </form>
+                <p>By subscribing you agree to our <a href='#'>Terms of Service</a> and <a href='#'>Privacy Policy</a></p>
+            </div>
+            <hr />
         </div>
     )
 }
--- src/js/slider_news.js
+++ src/js/slider_news.js
@@ -1,9 +1,67 @@
 import React from 'react';
+import NewsSlides from './objects/NewsSlides';
 const SliderNews = () => {
+    const [counter, setCounter] = React.useState(0);
+    console.log(counter);
+    const newslist = NewsSlides.map((news,index) => {
+        let new_img = require(`../images/${news.img}`);
+        let autor_img = require(`../images/${news.autor_img}`);
+        return(
+            <div key={news.id} style={{left:`${index*25}%`,transform:`translateX(${counter*100}%)`}} className='SliderNews__slidercontainer__slide'>
+                <div className='SliderNews__slidercontainer__slide__image'> 
+                    <img src={new_img} alt={news.title}/>
+                    <span>{news.tag}</span>
+                </div>
+                <div className='SliderNews__slidercontainer__slide__content'>
+                    <div className='SliderNews__slidercontainer__slide__content__title'>
+                        <h1>{news.title}</h1>
+                    </div>
+                    <div className='SliderNews__slidercontainer__slide__content__autor'>
+                        <div className='SliderNews__slidercontainer__slide__content__autor__image'>
+                            <img src={autor_img} alt={news.autor}/>
+                        </div>
+                        <div className='SliderNews__slidercontainer__slide__content__autor__name'>
+                            <p>{news.autor}</p>
+                        </div>
+                        <div className='SliderNews__slidercontainer__slide__content__autor__date'>
+                            <p>{news.date}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    })
+    
+    const handleCounter = (side) => {
+        const sign = side === 'left' ? -1 : 1;
+        setCounter(prevcounter => {
+            if(prevcounter === -2 && side === 'left'){
+                return prevcounter = 0;
+            }else if(prevcounter === 0 && side === 'right'){
+                return prevcounter = -newslist.length + 4;
+            }
+            return prevcounter = prevcounter + sign;
+        })
+    }
+    React.useEffect(() => {
+        const interval = setInterval(() => {
+            handleCounter('left');
+        }, 5000);
+        return () => clearInterval(interval);
+    }, []);
     return (
         <div className='SliderNews'>
-            
-            <h1><i class="fa-solid fa-bullhorn"></i>Sponsored news</h1>
+            <div className='SliderNews__title'>
+                <h1><i class="fa-solid fa-bullhorn"></i>Sponsored news</h1>
+                <p>News from our sponsors</p>
+            </div>
+            <div className='SliderNews__slidercontainer'>
+                {newslist}
+            </div>   
+            <div className='SliderNews__buttons'>
+                <i className="fa-solid fa-circle-chevron-left"onClick={ ()=>{handleCounter('right')}}  ></i>
+                <i className="fa-solid fa-circle-chevron-right" onClick={()=>{handleCounter('left')}}></i>
+            </div>
         </div>
     )
 }
